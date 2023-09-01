11fcef15332d9533bec49e22478bf0acc93af9ff
Sahand Ghavidel <sahand.gha@gmail.com>
2023-09-01 10:01:37 +0800

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return <h1 className='text-red-500'>App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>SignUp</div>
+  )
+}
