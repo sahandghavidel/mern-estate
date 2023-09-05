1a72acb35ee23267f8aa823897c9fb3c00db6bde
Sahand Ghavidel <sahand.gha@gmail.com>
2023-09-05 10:27:40 +0800

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-    }
-)
+  console.log('Server is running on port 3000!');
+});
